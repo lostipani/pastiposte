b6b39b914160d770edc880ded94a869509e18210
nocciola <nico.offeddu@gmail.com>
2025-10-08 19:18:20 +0200

    debug 1

--- src/analyst/analyst.py
+++ src/analyst/analyst.py
@@ -62,7 +62,7 @@ class Analyst(rabbitMQConsumer):
             id_binance=1,
             price=50e3,
             quantity=0.0004,
-            tracked_by=[analyst_1],
+            tracked_by=[os.getenv("ANALYST_ID", "analyst_1")],
         )
         broker.add(
             json.dumps(
